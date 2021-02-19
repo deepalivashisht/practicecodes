ffc495af24c7d1e9e2181dc7697a4e0197355225
deepalivashisht <46275604+deepalivashisht@users.noreply.github.com>
2021-02-19 18:44:05 +0530

    19 feb

--- prime.c
+++ prime.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int n,i,flag=0;
-printf("enter the number:");
-scanf("%d",&n);
-for(i=2;i<=n/2;i++)
-{
-if(n%i==0)
-{
-flag=1;
-printf("not prime");
-break;
-
-}
-}
-if(flag==0)
-printf("prime");
-return 0;
+             int n,i,flag=0;
+             printf("enter the number:");
+             scanf("%d",&n);
+             for(i=2;i<=n/2;i++)
+              {
+                         if(n%i==0)
+                          {
+                                 flag=1;
+                                 printf("not prime");
+                                 break;
+                           }
+               }
+             if(flag==0)
+                         printf("prime");
+             return 0;
 }
