29fba68f9b0d0d2eabbcbf5f4e573dae09b06b04
deepalivashisht <46275604+deepalivashisht@users.noreply.github.com>
2021-02-19 18:42:07 +0530

    19 feb

--- palindrone.c
+++ palindrone.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int n, num, rev=0, rem;
-printf("enter the number:");
-scanf("%d",&n);
-num=n;
-while(n!=0)
-{
-rem=n%10;
-rev=rev*10+rem;
-n/=10;
-}
-(num==rev)?printf("palindrone"):printf("not palindrone");
-return 0;
+            int n, num, rev=0, rem;
+            printf("enter the number:");
+            scanf("%d",&n);
+            num=n;
+            while(n!=0)
+            {
+                        rem=n%10;
+                        rev=rev*10+rem;
+                        n/=10;
+             }
+            (num==rev)?printf("palindrone"):printf("not palindrone");
+            return 0;
 }
