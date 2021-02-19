cfb5f40752ada1874a9ece1743a4eef2fa53ee5e
deepalivashisht <46275604+deepalivashisht@users.noreply.github.com>
2021-02-19 18:28:51 +0530

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
-float r,circum,area;
-cout<<"Enter the radius of circle"<<endl;
-cin>>r;
-circum=2*pie*r;
-area=pie*r*r;
-cout<<"Circumference of circle:"<<circum<<endl;
-cout<<"Area of circle:"<<area<<endl;
-return 0;
+            float r,circum,area;
+            cout<<"Enter the radius of circle"<<endl;
+            cin>>r;
+            circum=2*pie*r;
+            area=pie*r*r;
+            cout<<"Circumference of circle:"<<circum<<endl;
+            cout<<"Area of circle:"<<area<<endl;
+            return 0;
 }
 
