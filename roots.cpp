78d24b6197755c4abdd7c75776e71ca495d2b1c2
deepalivashisht <46275604+deepalivashisht@users.noreply.github.com>
2021-02-19 18:45:50 +0530

    19 feb

--- roots.cpp
+++ roots.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 int main()
 {
-double a,b,c,discriminant,root1,root2;
-cout<<"Enter the value of a,b and c";
-cin>>a>>b>>c;
-discriminant=(b*b)-(4*a*c);
-cout<<"Discriminant="<<discriminant<<endl;
-if(discriminant==0)
-cout<<"Quadratic equation has equal and real roots."<<endl;
-else if(discriminant>0)
-cout<<"Quadratic equation has unequal and real roots."<<endl;
-else 
-cout<<"Quadratic equation has imaginary roots."<<endl;
-root1=(-b+pow(discriminant,1/2))/(2*a);
-root2=(-b-pow(discriminant,1/2))/(2*a);
-cout<<"Roots of Quadratic equation are:"<<endl<<root1<<endl<<root2;
-return 0;
+                 double a,b,c,discriminant,root1,root2;
+                 cout<<"Enter the value of a,b and c";
+                 cin>>a>>b>>c;
+                 discriminant=(b*b)-(4*a*c);
+                 cout<<"Discriminant="<<discriminant<<endl;
+                 if(discriminant==0)
+                              cout<<"Quadratic equation has equal and real roots."<<endl;
+                 else if(discriminant>0)
+                              cout<<"Quadratic equation has unequal and real roots."<<endl;
+                 else 
+                              cout<<"Quadratic equation has imaginary roots."<<endl;
+                 root1=(-b+pow(discriminant,1/2))/(2*a);
+                 root2=(-b-pow(discriminant,1/2))/(2*a);
+                              cout<<"Roots of Quadratic equation are:"<<endl<<root1<<endl<<root2;
+                 return 0;
 }
 
